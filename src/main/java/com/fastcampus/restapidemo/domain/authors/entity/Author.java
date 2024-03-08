fec5202f2ad055856c98b9df85c011c571b56bed
2inlee <kbs@kakao.com>
2024-03-08 19:21:58 +0900

    add entity all

--- src/main/java/com/fastcampus/restapidemo/domain/authors/entity/Author.java
+++ src/main/java/com/fastcampus/restapidemo/domain/authors/entity/Author.java
@@ -1,2 +1,29 @@
-package com.fastcampus.restapidemo.domain.authors.entity;public class Author {
+package com.fastcampus.restapidemo.domain.authors.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.awt.print.Book;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "authors")
+    @JsonBackReference
+    private Set<Book> books = new HashSet<>();
 }
--- src/main/java/com/fastcampus/restapidemo/domain/books/entity/Book.java
+++ src/main/java/com/fastcampus/restapidemo/domain/books/entity/Book.java
@@ -1,4 +1,48 @@
 package com.fastcampus.restapidemo.domain.books.entity;
 
-public class Books {
+import com.fastcampus.restapidemo.domain.authors.entity.Author;
+import com.fastcampus.restapidemo.domain.reviews.entity.Review;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String publisher;
+
+    @Column(name = "published_date")
+    @Temporal(TemporalType.DATE)
+    private Date publishedDate;
+
+    @ManyToMany
+    @JoinTable(
+        name = "book_author",
+        joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+
+    @JsonManagedReference
+    private Set<Author> authors = new HashSet<>();
+
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @JsonManagedReference
+    private Set<Review> reviews = new HashSet<>();
 }
